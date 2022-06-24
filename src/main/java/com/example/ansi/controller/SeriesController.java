7c0bc974588e0ce44c06b48f30dfb772b829b4ca
PaganMuffin <jan.rg93@gmail.com>
2022-06-24 02:40:26 +0200

    add anilist search by title

---
+++ src/main/java/com/example/ansi/controller/SeriesController.java
@@ -0,0 +1,16 @@
+package com.example.ansi.controller;
+
+import com.example.ansi.utills.AniList;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SeriesController {
+    @GetMapping("/search")
+    public String searchAniList(@RequestParam String q, @RequestParam(required = false) String p) throws UnirestException {
+        p = p == null ? "1" : p;
+        return AniList.searchByTitle(q,p);
+    }
+}
---
+++ src/main/java/com/example/ansi/utills/AniList.java
@@ -0,0 +1,71 @@
+package com.example.ansi.utills;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import net.minidev.json.JSONObject;
+
+public class AniList {
+
+    static String GraphQLURL = "https://graphql.anilist.co";
+
+    public static String searchByTitle(String q, String p) throws UnirestException {
+        String GraphQLQuery = """
+                    query ($id: Int, $page: Int, $perPage: Int, $search: String) {
+                    Page (page: $page, perPage: $perPage) {
+                        pageInfo {
+                            total
+                                    currentPage
+                            lastPage
+                                    hasNextPage
+                            perPage
+                        }
+                        media (id: $id, search: $search, type: ANIME) {
+                            id
+                            idMal
+                            title {
+                                romaji
+                            }
+                            coverImage{
+                                medium
+                            }
+                            type
+                                    season
+                            seasonYear
+                                    description
+                            episodes
+                        }
+                    }
+                }""";
+
+        JSONObject variables = new JSONObject();
+        variables.put("page", p);
+        variables.put("perPage", 10);
+        variables.put("search", q);
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("query", GraphQLQuery);
+        jsonObject.put("variables", variables);
+
+        HttpResponse<JsonNode> response = Unirest.post(GraphQLURL)
+                .header("Content-Type", "application/json")
+                .header("Accept", "application/json")
+                .body(jsonObject.toJSONString())
+                .asJson();
+
+        //Prettifying
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        JsonParser jp = new JsonParser();
+        JsonElement je = jp.parse(response.getBody().toString());
+        return gson.toJson(je);
+    }
+
+    //GET by ID
+
+
+}
